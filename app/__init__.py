4eac4cbde69de511b8f8c1c05f6440cbd5cedfee
Denis <counter297@gmail.com>
2020-10-10 14:25:45 +0200

    test3

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template, Response
+#from config import Config
 
-print("__init__.py file started")
+#print(__name__)
 app = Flask(__name__)
+#app.config.from_object(Config)
 
 from app import routes
