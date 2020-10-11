a7df767812ee6410d963b202390ca9785689505a
Denis <counter297@gmail.com>
2020-10-11 23:51:32 +0200

    base.html fix

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,18 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username': 'guest'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }, 
+        {
+            'author': {'username': 'admin'},
+            'body': 'website test commencing....'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
