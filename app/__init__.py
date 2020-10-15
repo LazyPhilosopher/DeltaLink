60c78a23f9947c6618fd56c2a9b11c3e8ff796b0
Denis <counter297@gmail.com>
2020-10-15 19:14:35 +0200

    Database routine. Not working yet.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, Response
 from config import Config							#configuration file declared (SECRET_KEY)
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 #print(__name__)
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)						# database
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '123456890'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')                          # Using environment variable 'DATABASE_URL' as database adress. 
+                                                                                # If not defined default adress '/app.db' is used and stored into basedir variable. 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
