e15c30bdf02936a47bb8334d8bca3df4adb310ba
Denis <counter297@gmail.com>
2020-10-17 21:14:49 +0200

    Registration form done. At least working locally -_-

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template, Response
 from config import Config							#configuration file declared (SECRET_KEY)
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 #print(__name__)
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)						# database
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 from app import routes, models
